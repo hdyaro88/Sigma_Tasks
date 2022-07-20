a5dbe9cd60b3bff5cd84edf10c39ff654056dba7
hdyaro77 <harshit.dubey1923@gmail.com>
2022-07-21 02:29:20 +0530

    deploy

--- server.js
+++ server.js
@@ -3,14 +3,15 @@ const app = require("./app");
 const server = http.createServer(app);
 const path = require("path");
 const port = process.env.PORT || 8080;
-
+var cors = require("cors");
+app.use(cors());
 if (process.env.NODE_ENV === "production") {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, "client/build")));
+  app.use(express.static(path.join(__dirname, "/client/build")));
   // Handle React routing, return all requests to React app
 
   app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+    res.sendFile(path.join(__dirname, "/client/build", "index.html"));
   });
 }
 
